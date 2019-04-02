b5bad1dbd537b4a72eda410658be35872748f394
visualstudio <visualstudio@125-PC11>
2019-04-02 17:12:44 -0400

    m

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp7
+// student name Anmoldeep Singh
+// student name Simranjeet Kaur
+// student Id C0726122
+// student Id C0730317
+// Csd3354 Section 2
+//Assignment 2
+// march 6,2019
+namespace DelegatesAndEvents
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+        }
+    }
+
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate();
+
+        void Method1()
+        {
+            System.Console.WriteLine("MyDelegate");
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
